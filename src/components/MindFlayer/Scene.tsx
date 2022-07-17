461b15ff34aa6a3453e479aa4e7f91744d5cf82a
Bezik1 <mateusz.adamowicz.lo1@gmail.com>
2022-07-17 22:13:43 +0200

    Mind flayer update
    Add pointerlight whichs moves when cursor moves too

--- src/components/MindFlayer/Scene.tsx
+++ src/components/MindFlayer/Scene.tsx
@@ -1,14 +1,19 @@
 import { OrbitControls } from "@react-three/drei"
 import { Canvas } from "@react-three/fiber"
+import { useState } from "react"
 import Object from './Object'
 
 const Scene = () =>{
+    const [pos, setPos] = useState<number[]>([])
+
+    const handleMove = (e: React.PointerEvent<HTMLDivElement>) =>{
+        e.clientX && e.clientY ? setPos([e.clientX - 200, e.clientY - 400]) : setPos([-200, -400])
+    }
+
     return (
-    <Canvas>
+    <Canvas onPointerMove={e => handleMove(e)}>
         <Object/>
-        <pointLight color="violet" intensity={100} position={[0, 0, 0]} /> 
-        <pointLight color="violet" intensity={100} position={[0, 0, -30]} /> 
-        <pointLight color="violet" intensity={100} position={[0, -30, 0]} /> 
+        <pointLight color="violet" intensity={100} position={[pos[1], pos[1], pos[0]]} /> 
         <OrbitControls target={[0, -20, 0]} enableRotate={false} />
     </Canvas>
     )
