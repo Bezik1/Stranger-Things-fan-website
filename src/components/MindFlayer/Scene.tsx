2ffcdd08657e195d623d8ed3a27a2e9bfaed3feb
Bezik1 <mateusz.adamowicz.lo1@gmail.com>
2022-07-18 16:47:41 +0200

    Mind Flayer lights update
    More smoothy light transition effect

--- src/components/MindFlayer/Scene.tsx
+++ src/components/MindFlayer/Scene.tsx
@@ -7,13 +7,15 @@ const Scene = () =>{
     const [pos, setPos] = useState<number[]>([])
 
     const handleMove = (e: React.PointerEvent<HTMLDivElement>) =>{
-        e.clientX && e.clientY ? setPos([e.clientX - 200, e.clientY - 400]) : setPos([-200, -400])
+        e.clientX && e.clientY ? setPos([e.clientX - 200, e.clientY - 400]) : setPos([0, 300])
     }
 
     return (
     <Canvas onPointerMove={e => handleMove(e)}>
         <Object/>
-        <pointLight color="violet" intensity={100} position={[pos[1], pos[1], pos[0]]} /> 
+        <pointLight color="violet" intensity={100} position={[0 + pos[1], 0 + pos[1], 0 + pos[0]]} /> 
+        <pointLight color="violet" intensity={100} position={[0, 0, -30]} /> 
+        <pointLight color="violet" intensity={100} position={[0, -30, 0]} /> 
         <OrbitControls target={[0, -20, 0]} enableRotate={false} />
     </Canvas>
     )
